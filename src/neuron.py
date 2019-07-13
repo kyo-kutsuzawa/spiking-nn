a2103962e019f6977a4a21323b8ecbf2ec0ab7e0
Kyo Kutsuzawa <k.kutsuzawa.430@ms.saitama-u.ac.jp>
2019-07-13 16:42:58 +0900

    Add neuron.py

---
+++ src/neuron.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class IzhikevichNeuron:
+    def __init__(self):
+        pass
+
+    def reset_state(self):
+        pass
+
+    def update(self, i):
+        pass
+
+
+def example_izhikevic():
+    pass
+
+
+if __name__ == "__main__":
+    example_izhikevic()
